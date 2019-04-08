224b7aeebc89164ca67b15336e23c11f5641b0fe
Thijs Schreijer <thijs@thijsschreijer.nl>
2019-04-08 10:59:43 +0200

    more idiomatic Lua

--- handler.lua
+++ handler.lua
@@ -6,6 +6,7 @@
 
 -- Grab pluginname from module name
 local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
+local random = math.random
 
 -- load the base plugin object and create a subclass
 local plugin = require("kong.plugins.base_plugin"):extend()
@@ -13,7 +14,7 @@ local plugin = require("kong.plugins.base_plugin"):extend()
 -- constructor
 function plugin:new()
   plugin.super.new(self, plugin_name)
-  
+
   -- do initialization here, runs in the 'init_by_lua_block', before worker processes are forked
 
 end
@@ -32,60 +33,51 @@ end
 --[[ handles more initialization, but AFTER the worker process has been forked/created.
 -- It runs in the 'init_worker_by_lua_block'
 function plugin:init_worker()
-  plugin.super.access(self)
+  plugin.super.init_worker(self)
 
   -- your custom code here
-  
+
 end --]]
 
 --[[ runs in the ssl_certificate_by_lua_block handler
 function plugin:certificate(plugin_conf)
-  plugin.super.access(self)
+  plugin.super.certificate(self)
 
   -- your custom code here
-  
+
 end --]]
 
 --[[ runs in the 'rewrite_by_lua_block' (from version 0.10.2+)
 -- IMPORTANT: during the `rewrite` phase neither the `api` nor the `consumer` will have
--- been identified, hence this handler will only be executed if the plugin is 
+-- been identified, hence this handler will only be executed if the plugin is
 -- configured as a global plugin!
 function plugin:rewrite(plugin_conf)
   plugin.super.rewrite(self)
 
   -- your custom code here
-  
+
 end --]]
 
 ---[[ runs in the 'access_by_lua_block'
 function plugin:access(config)
   plugin.super.access(self)
-  local percentage_random = math.random (100)
-  if config.request_percentage > percentage_random then
-    local latency_diff = config.maximum_latency_msec - config.minimum_latency_msec
-    local latency = 0
-    if latency_diff < 0 then 
-      latency = 0
-    else
-      latency = config.minimum_latency_msec + math.random(latency_diff)
-    end
-    if config.add_header == true then
-      ngx.header["X-Kong-Random-Latency"] = latency
-    end
-    if latency > 0 then
-      ngx.sleep(latency/1000)
-    end
-  else
-    if config.add_header == true then
-      ngx.header["X-Kong-Random-Latency"] = "none"
-    end
-  end 
-  
+
+  local latency
+  if config.request_percentage > random(100) then
+    latency = config.minimum_latency_msec +
+              random(config.minimum_latency_msec, config.maximum_latency_msec)
+    ngx.sleep(latency/1000)
+  end
+
+  if config.add_header then
+    ngx.header["X-Kong-Random-Latency"] = latency or "none"
+  end
+
 end --]]
 
----[[ runs in the 'header_filter_by_lua_block'
+--[[ runs in the 'header_filter_by_lua_block'
 function plugin:header_filter(plugin_conf)
-  plugin.super.access(self)
+  plugin.super.header_filter(self)
 
   -- your custom code here, for example;
 
@@ -93,18 +85,18 @@ end --]]
 
 --[[ runs in the 'body_filter_by_lua_block'
 function plugin:body_filter(plugin_conf)
-  plugin.super.access(self)
+  plugin.super.body_filter(self)
 
   -- your custom code here
-  
+
 end --]]
 
 --[[ runs in the 'log_by_lua_block'
 function plugin:log(plugin_conf)
-  plugin.super.access(self)
+  plugin.super.log(self)
 
   -- your custom code here
-  
+
 end --]]
 
 
--- schema.lua
+++ schema.lua
@@ -1,52 +1,29 @@
+local Errors = require "kong.dao.errors"
+
 local function check_percentage(given_value, given_config)
-  local percentage = tonumber(given_value)
-  if percentage == nil then
+  local percentage = tonumber(given_value) or -1
+  if percentage < 0 or percentage > 100 then
     return false, "Only numbers between 0 and 100"
   end
 
-  if percentage > 100 then
-    return false, "Maximum percentage is 100"
-  end
-
-  if percentage < 0 then
-    return false, "Minimum percentage is 0"
-  end
-
-  given_config.request_percentage=percentage
+  given_config.request_percentage = percentage
 end
 
 local function check_minimum(given_value, given_config)
-  local minimum = tonumber(given_value)
-  if minimum == nil then
-    return false, "Only numbers allowed"
-  end
-
-
+  local minimum = tonumber(given_value) or -1
   if minimum < 0 then
-    return false, "Minimum latency must not be smaller than 0"
+    return false, "Minimum latency must be a number greater than or equal to 0"
   end
-
-  --if minimum > given_config.maximum_latency_msec then
-  --  return false, "Minimum latency must not be bigger than maximum latency"
-  --end
 
   given_config.minimum_latency_msec = minimum
 end
 
 local function check_maximum(given_value, given_config)
-  local maximum = tonumber(given_value)
-  if maximum == nil then
-    return false, "Only numbers allowed"
-  end
-
+  local maximum = tonumber(given_value) or -1
 
   if maximum < 0 then
-    return false, "Maximum latency must not be smaller than 0"
+    return false, "Maximum latency must be a number greater than or equal to 0"
   end
-
-  --if maximum < given_config.minimum_latency_msec then
-  --  return false, "Minimum latency must not be bigger than maximum latency"
-  --end
 
   given_config.maximum_latency_msec = maximum
 end
@@ -59,10 +36,16 @@ return {
     maximum_latency_msec = {type = "integer", required = true, func = check_maximum, default = 1000},
     request_percentage = {type = "integer", required = true, func = check_percentage, default = 50},
     add_header = {type = "boolean", default = true},
-    
+
   },
   self_check = function(schema, plugin_t, dao, is_updating)
-    -- perform any custom verification
+    local maximum = tonumber(plugin_t.maximum_latency_msec) or -1
+    local minimum = tonumber(plugin_t.minimum_latency_msec) or -1
+
+    if minimum > maximum then
+      return false, Errors.schema "minimum latency must be smaller than maximum latency"
+    end
+
     return true
   end
 }
